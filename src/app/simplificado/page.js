dae868eccd457b1923442f58576b7e50be6738a3
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-08-20 22:21:21 -0500

    ✨Update page and  layouts.

--- src/app/simplificado/page.js
+++ src/app/simplificado/page.js
@@ -1,7 +1,7 @@
-export default function Exonerado() {
+export default function Simplificado() {
     return (
         <section className="mt-40 w-full h-full border-neutral-300 bg-white p-3 px-11 placeholder-neutral-500 focus:outline-none     focus:ring-1 focus:ring-neutral-300  dark:bg-background dark:focus:ring-neutral-700 flex flex-col gap-4">
-            Proceso Exonerado
+            Proceso Simplificado
         </section>
     )
 }
