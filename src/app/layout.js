75c52393e9ba4afd87277f0ab25e3c8dee805c6b
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-09-09 00:00:12 -0500

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
                     </footer>
                 </ThemeProvider>
 
-                <div className="absolute top-0 z-[-2] h-screen w-full  bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.2),rgba(255,255,255,0))]"></div>
+                <div className="absolute top-0 z-[-2] h-screen w-full  bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.1),rgba(255,255,255,0))]"></div>
             </body>
         </html>
     )
