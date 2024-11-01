1c7acb6e59cb57102fda198b8476534edef247f6
Ivan Rodrigo <ivan.rodrigo.mayta@gmail.com>
2024-11-01 16:41:24 -0500

    Refactor: highlighText to main title

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,25 @@
 import { Tracking } from "../components/tracking-dhl"
 import { Poppins } from "next/font/google"
 import { Suspense } from "react"
+import { highlightedText } from "../lib/utils"
+const poppins = Poppins({ subsets: ["latin"], weight: "700" })
 
-const poppins = Poppins({ subsets: ["latin"], weight: "600" })
+const highlighText = {
+    text: "Cotiza y Rastrea tus envios de forma rapida y sencilla",
+    keywords: ["Cotiza", "Rastrea"],
+    styles: "text-transparent bg-clip-text bg-gradient-to-tr from-yellow-500 to-orange-500",
+}
 export default function Home() {
+    const { text, keywords, styles } = highlighText
+
     return (
         <>
             <section className="relative mt-40 w-full h-full border-neutral-300 p-3 px-12 placeholder-neutral-500 focus:outline-none   focus:ring-1 focus:ring-neutral-300 dark:focus:ring-neutral-700 flex flex-col gap-4">
-                {/* <span className="absolute bottom-1/4 left-1/4 -translate-x-1/2 shadow-lg animate-float">
-                <IconDhl className="w-16 fill-yellow-500"></IconDhl>
-            </span> */}
-
                 <h1
-                    className={`${poppins.className}  mb-10 sm:mb-20 text-3xl text-center sm:text-6xl dark:text-white text-stone-800 `}
+                    className={`${poppins.className} mb-10 sm:mb-20 text-3xl text-center sm:text-6xl dark:text-white text-stone-800`}
                 >
-                    <span className="text-transparent bg-clip-text bg-gradient-to-tr from-yellow-500 to-orange-500">
-                        Cotiza{" "}
-                    </span>
-                    y {""}
-                    <span className="text-transparent bg-clip-text bg-gradient-to-tr from-yellow-500 to-orange-500">
-                        Rastrea{" "}
-                    </span>
-                    tus envios de forma rapida y sencilla
+                    {highlightedText(text, keywords, styles)}
                 </h1>
-
                 <Suspense fallback={<div>Loading...</div>}>
                     <Tracking />
                 </Suspense>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,5 +2,19 @@ import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function highlightedText(text, keywords, styles) {
+    return text.split(" ").map((word, index) => {
+        const isKeyword = keywords.includes(word)
+        return (
+            <span
+                key={index}
+                className={cn(isKeyword && styles, index > 0 && "ml-1")}
+            >
+                {word}{" "}
+            </span>
+        )
+    })
 }
