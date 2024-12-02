b4a73584c30cbff2235eca0a2a73097a11c487e3
Ivan Rodrigo <ivan.rodrigo.mayta@gmail.com>
2024-12-02 17:59:21 -0500

    feat: add exchange component

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,8 @@
 import { Tracking } from "@components/shipment/tracking-dhl"
 import { Suspense } from "react"
 import { highlightedText } from "@/lib/utils"
-import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardContent,
-} from "@/components/ui/card"
-import { ExchangeFill } from "@/icons/exchange-fill"
+import Exchange from "@/components/exchange"
+import Features from "@/components/features"
 
 const highlighText = {
     text: "Cotiza y Rastrea tus envios de forma rapida y sencilla",
@@ -23,42 +18,12 @@ export default function Home() {
                 <h2 className="lg:w-3/5 text-4xl sm:text-5xl font-bold">
                     {highlightedText(text, keywords, styles)}
                 </h2>
-                <Card className="dark:bg-exchange lg:w-2/5 border-[0px]">
-                    <CardHeader>
-                        <CardDescription className="text-center">
-                            Tipo de cambio
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="relative flex flex-col gap-2 ">
-                        <div className=" h-12 border-2 rounded-lg flex">
-                            <input
-                                className="h-full rounded-l-lg flex-grow"
-                                text=""
-                            ></input>
-                            <label className="w-1/3 h-full bg-black text-white px-2 rounded-r-lg flex items-center justify-center">
-                                Dolares
-                            </label>
-                        </div>
-                        <div className="absolute top-[40%] right-[30%] -translate-y-1/3 -translate-x-1/3">
-                            <div className="bg-background/40 w-6 h-6 rounded-full">
-                                <ExchangeFill  />
-                            </div>
-                        </div>
-                        <div className="h-12 border-2 rounded-lg flex">
-                            <input
-                                className="h-full rounded-l-lg flex-grow"
-                                text=""
-                            ></input>
-                            <label className="w-1/3 h-full bg-black text-white px-2 rounded-r-lg flex items-center justify-center">
-                                Soles
-                            </label>
-                        </div>
-                    </CardContent>
-                </Card>
+                <Exchange />
             </section>
             <Suspense fallback={<div>Loading...</div>}>
                 <Tracking />
             </Suspense>
+            <Features />
         </>
     )
 }
---
+++ src/components/exchange.tsx
@@ -0,0 +1,39 @@
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardContent,
+} from "@/components/ui/card"
+import { ExchangeFill } from "@/icons/exchange-fill"
+
+function Exchange() {
+    return (
+        <Card className="dark:bg-exchange lg:w-2/5 border-[0px]">
+            <CardHeader>
+                <CardDescription className="text-center">
+                    Tipo de cambio
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="relative flex flex-col gap-2 ">
+                <div className=" h-12 border-2 rounded-lg flex">
+                    <input value={1} className="h-full rounded-l-lg flex-grow pl-6"></input>
+                    <label className="w-1/3 h-full bg-black text-white px-2 rounded-r-lg flex items-center justify-center">
+                        Dolares
+                    </label>
+                </div>
+                <div className="absolute top-[40%] right-[30%] -translate-y-1/3 -translate-x-1/3">
+                    <div className="bg-background/40 w-6 h-6 rounded-full">
+                        <ExchangeFill />
+                    </div>
+                </div>
+                <div className="h-12 border-2 rounded-lg flex">
+                    <input value={3.84} className="h-full rounded-l-lg flex-grow pl-6"></input>
+                    <label className="w-1/3 h-full bg-black text-white px-2 rounded-r-lg flex items-center justify-center">
+                        Soles
+                    </label>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+export default Exchange
