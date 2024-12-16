3b2631e24fd2e6a6ac324d8f4490789788ebf75f
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2024-12-16 14:19:49 -0500

    refactor: reemplazar ListItem por NavigationMenuLink en el componente Header para mejorar la semántica y el estilo

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -60,14 +60,25 @@ export default function Header() {
                                 </NavigationMenuTrigger>
                                 <NavigationMenuContent>
                                     <ul className="grid w-[400px] gap-3 p-4 md:w-[400px] md:grid-cols-2 lg:w-[500px] ">
-                                        {components.map((component) => (
-                                            <ListItem
-                                                key={component.title}
-                                                title={component.title}
-                                                href={component.href}
+                                        {components.map((component, key) => (
+                                            <NavigationMenuLink
+                                                asChild
+                                                key={key}
                                             >
-                                                {component.description}
-                                            </ListItem>
+                                                <Link
+                                                    href={component.href}
+                                                    className={cn(
+                                                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                                                    )}
+                                                >
+                                                    <div className="text-sm font-medium leading-none">
+                                                        {component.title}
+                                                    </div>
+                                                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+                                                        {component.description}
+                                                    </p>
+                                                </Link>
+                                            </NavigationMenuLink>
                                         ))}
                                     </ul>
                                 </NavigationMenuContent>
@@ -81,31 +92,3 @@ export default function Header() {
         </header>
     )
 }
-
-const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">
-                        {title}
-                    </div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    )
-})
-ListItem.displayName = "ListItem"
