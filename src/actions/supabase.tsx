1ef43301a8aa1dfdd07954420f3910eef0d333fe
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2025-03-02 22:47:10 -0500

    feat: add optional RUC field to profile schema and improve product validation

--- src/actions/supabase.tsx
+++ src/actions/supabase.tsx
@@ -33,6 +33,7 @@ const createProfileFormSchema = z.object({
         .max(160, {
             message: "La descripción no debe ser mayor a 160 caracteres.",
         }),
+    RUC: z.string().optional(),
     username: z
         .string()
         .min(2, {
@@ -60,7 +61,7 @@ const createProfileFormSchema = z.object({
             message: "La dirección debe ser de al menos 4 caracteres.",
         }),
 })
-//TODO make zod validation with optional fields
+//[x]TODO make zod validation for update and create profile
 
 export const updateProfile = async (
     prevState: State,
@@ -82,6 +83,7 @@ export const updateProfile = async (
             tiktok: formData.get("social_urls[tiktok]"),
         },
     })
+    //console.log(validatedFields.data)
     if (!validatedFields.success) {
         return {
             message: "There was a problem creating your profile.",
@@ -149,6 +151,7 @@ export const createProfile = async (
             errors: validatedFields.error.flatten().fieldErrors,
         }
     }
+    //console.log(validatedFields.data)
     const { error, statusText, status } = await supabase
         .from("profile")
         .insert({
@@ -182,23 +185,60 @@ export const createProfile = async (
         errors: {},
     }
 }
-//TODO make zod validation with optional fields
+//TODO make zod validation for add product
+//TODO [ ] fix price error
 const productFormSchema = z.object({
-    name: z.string().min(2).max(100),
-    description: z.string().min(2).max(500),
-    price: z.number().min(0),
-    image_url: z.string().url(),
+    name: z
+        .string()
+        .min(4, {
+            message: "El nombre debe ser de al menos 4 caracteres",
+        })
+        .max(100, {
+            message: "El nombre no debe ser mayor a 100 caracteres",
+        }),
+    description: z
+        .string()
+        .min(2, {
+            message: "La descripción debe ser de al menos 2 caracteres",
+        })
+        .max(230, {
+            message: "La descripción no debe ser mayor a 230 caracteres",
+        }),
+    price: z.number().min(0, { message: "El precio debe ser mayor a 0" }),
+    image_url: z.string().url({
+        message: "La URL de la imagen no es válida",
+    }),
 })
-export const addProduct = async (formData: FormData) => {
+
+export type ProductState = {
+    errors?: {
+        name?: string[]
+        description?: string[]
+        price?: string[]
+        image_url?: string[]
+    }
+    message?: string | null
+}
+export const addProduct = async (
+    prevState: ProductState,
+    formData: FormData
+): Promise<ProductState> => {
     const supabase = await createClient()
     const { data: userData, error: userError } = await supabase.auth.getUser()
     if (userError) {
         console.error("Error getting user:", userError.message)
 
-        return
+        return {
+            message: "Error getting user",
+            errors: {},
+        }
     }
     const user_id = userData.user.id
-    const { data: perfilData, error: perfilError } = await supabase
+    const {
+        data: perfilData,
+        error: perfilError,
+        status,
+    } = await supabase
         .from("profile")
         .select("id")
         .eq("user_id", user_id)
@@ -206,11 +246,19 @@ export const addProduct = async (formData: FormData) => {
     if (perfilError) {
         console.error("Error getting profile:", perfilError.message)
 
-        return
+        return {
+            message: "Profile not found",
+            errors: {},
+        }
     }
     const perfil_id = perfilData.id
     // Prepare product data
-
+    const validatedFields = productFormSchema.safeParse({
+        name: formData.get("name"),
+        description: formData.get("description"),
+        price: Number(formData.get("price")),
+        image_url: formData.get("image_url"),
+    })
     const productData = {
         perfil_id: perfil_id,
 
@@ -220,10 +268,18 @@ export const addProduct = async (formData: FormData) => {
         image_url: formData.get("image_url"),
     }
     // Insert the product
-
+    console.log(validatedFields.data)
+    if (!validatedFields.success) {
+        return {
+            message: "Hubo un problema al crear el producto.",
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+    console.log(validatedFields.data)
     const { data: productDataResponse, error: productError } = await supabase
         .from("product")
-        .insert([productData])
+        .insert({ ...validatedFields.data, perfil_id: perfil_id })
+
     if (productError) {
         console.error("Error creating product:", productError.message)
         return {
