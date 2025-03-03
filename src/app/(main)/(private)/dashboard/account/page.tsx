865140e1a1f77362c09af785e2aa58833979a858
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2025-03-02 22:50:20 -0500

    refactor: improve and adapt product form for validations.

--- src/app/(main)/(private)/dashboard/account/page.tsx
+++ src/app/(main)/(private)/dashboard/account/page.tsx
@@ -2,24 +2,25 @@ import { createClient } from "@/utils/supabase/server"
 export default async function SettingsAccountPage() {
     const supabase = await createClient()
     const user = await supabase.auth.getUser()
-    console.log(user)
+    console.dir(user, { depth: null })
     return (
         <div className="space-y-6">
             <div>
-                <h3 className="text-lg font-medium">Account</h3>
+                <h3 className="text-2xl font-bold">Información de la Cuenta</h3>
                 <p className="text-sm text-muted-foreground">
                     Información de inicio de sesión
                 </p>
             </div>
+
             <div className="flex flex-col gap-2">
                 <div>
-                    <h3>Nombre</h3>
+                    <h3 className="font-semibold">Nombre</h3>
                     <p className="text-sm text-muted-foreground">
                         {user.data.user.user_metadata.name}
                     </p>
                 </div>
                 <div>
-                    <h3> email</h3>
+                    <h3 className="font-semibold"> email</h3>
                     <p className="text-sm text-muted-foreground">
                         {user.data.user.email}
                     </p>
--- src/app/(main)/(private)/dashboard/products/product-form.tsx
+++ src/app/(main)/(private)/dashboard/products/product-form.tsx
@@ -4,12 +4,19 @@ import { addProduct } from "@/actions/supabase"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { convertBlobUrlToFile } from "@/lib/utils"
+import { cn, convertBlobUrlToFile } from "@/lib/utils"
 import { getAuth, useGetUser } from "@/utils/supabase/auth/client"
 import { Toast } from "@radix-ui/react-toast"
 import { FileImageIcon } from "lucide-react"
 import Image from "next/image"
-import { ChangeEvent, useRef, useState, useTransition } from "react"
+import {
+    ChangeEvent,
+    useActionState,
+    useEffect,
+    useRef,
+    useState,
+    useTransition,
+} from "react"
 import toast from "react-hot-toast"
 export default function ProductForm() {
     const user = useGetUser()
@@ -17,7 +24,11 @@ export default function ProductForm() {
     const [imageUrls, setImageUrls] = useState<string[]>([])
     const [imageUrlUploaded, setImageUrlUploaded] = useState<string[]>([])
     const inputRef = useRef<HTMLInputElement>(null)
-
+    const initialState = { message: null, errors: {} }
+    const [state, formAction, pending] = useActionState(
+        addProduct,
+        initialState
+    )
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             const filesArray = Array.from(e.target.files)
@@ -58,78 +69,148 @@ export default function ProductForm() {
                 urls.push(imageUrl)
             }
             setImageUrlUploaded(urls)
-            console.log("urls", urls)
+            console.log(imageUrlUploaded)
+            //console.log("urls", urls)
             const auth = getAuth()
             await auth.updateUser({})
             toast.success("Successfully uploaded image")
             setImageUrls([])
         })
     }
+
     const handleSubmit = async (formData: FormData) => {
         formData.append("image_url", imageUrlUploaded[0])
-        const { message } = await addProduct(formData)
-        toast.success(message)
-        console.log("Product added")
+        formAction(formData)
+        //toast.success(message)
+        //toast.custom(state.message)
+        //console.log("Product added")
         setImageUrlUploaded([])
     }
+    useEffect(() => {
+        if (state?.message) {
+            toast.custom(`${state.message}`)
+        }
+        if (state.message === "Profile not found") {
+            toast.error("Perfil no encontrado. Por favor, complete su perfil")
+        }
+    }, [state])
+    console.log(state)
     return (
         <form className="flex gap-3 flex-col pt-8" action={handleSubmit}>
-            <Label>Titulo</Label>
-            <Input name="name" placeholder="Product Name" required />
-            <Label>Descripcion</Label>
-            <Input
-                name="description"
-                placeholder="Product Description"
-                required
-            />
-            <Label>Precio</Label>
-            <Input name="price" placeholder="Product Price" required />
-            <input
-                type="file"
-                multiple
-                hidden
-                ref={inputRef}
-                onChange={handleChange}
-                aria-describedby="file_input_help"
-                required
-            />
-            <Label>Imagen</Label>
-            <div className="flex items-center flex-col justify-center border rounded-md p-2 gap-2">
-                <button
-                    className=" w-full border flex flex-col items-center justify-center gap-1 py-4 "
-                    onClick={() => {
-                        inputRef.current.click()
-                    }}
-                >
-                    <FileImageIcon />
-                    Seleccionar Imagenes
-                </button>
-                <div className="grid  w-full  grid-cols-1 md:grid-cols-2 gap-1">
-                    {imageUrls.map((url) => (
-                        <Image
-                            key={url}
-                            src={url}
-                            width={300}
-                            height={300}
-                            alt={`image-${url}`}
-                            className="w-full h-auto aspect-square object-cover"
-                        />
-                    ))}
-                </div>
-            </div>
+            <ul className="flex flex-col gap-3">
+                <li>
+                    <input
+                        type="file"
+                        multiple
+                        hidden
+                        ref={inputRef}
+                        onChange={handleChange}
+                        aria-describedby="file_input_help"
+                    />
+                    <Label className="font-semibold">Imagen</Label>
+                    <div className="flex items-center flex-col justify-center border rounded-md p-2 gap-2">
+                        {imageUrls.length < 3 ? (
+                            <button
+                                className=" w-full border flex flex-col items-center justify-center gap-1 py-4 "
+                                onClick={() => {
+                                    inputRef.current.click()
+                                }}
+                            >
+                                <FileImageIcon />
+                                Seleccionar Imagenes
+                            </button>
+                        ) : null}
 
-            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">
-                SVG, PNG, JPG (MAX. 800x400px).
-            </p>
-            <Button
-                variant="link"
-                className="border p-2 "
-                type="button"
-                onClick={handleClickUploadImagesButton}
-                disabled={isPending}
-            >
-                {isPending ? "Subiendo..." : "Subir Imagen"}
-            </Button>
+                        <div className="w-full grid grid-cols-[repeat(auto-fit,_minmax(200px,1fr))] gap-4">
+                            {imageUrls.map((url) => (
+                                <Image
+                                    key={url}
+                                    src={url}
+                                    width={200}
+                                    height={200}
+                                    alt={`image-${url}`}
+                                    className="w-48 h-auto aspect-square object-cover"
+                                />
+                            ))}
+                            {imageUrlUploaded.map((url) => (
+                                <Image
+                                    key={url}
+                                    src={url}
+                                    width={200}
+                                    height={200}
+                                    alt={`image-${url}`}
+                                    className="w-48 h-auto aspect-square object-cover"
+                                />
+                            ))}
+                        </div>
+                    </div>
+
+                    <p
+                        className={cn("text-sm", {
+                            "text-red-500": state?.errors?.image_url,
+                            "text-slate-500": !state?.errors?.image_url,
+                        })}
+                    >
+                        {state?.errors?.image_url
+                            ? state.errors?.image_url
+                            : "Suba la imagen antes de añadir el producto. SVG, PNG, JPG (MAX. 800x400px)"}
+                    </p>
+                    <Button
+                        variant="link"
+                        className="border p-2 "
+                        type="button"
+                        onClick={handleClickUploadImagesButton}
+                        disabled={isPending}
+                    >
+                        {isPending ? "Subiendo..." : "Subir Imagen"}
+                    </Button>
+                </li>
+                <li>
+                    <Label className="font-semibold">Titulo</Label>
+                    <Input name="name" placeholder="Product Name" />
+                    <p
+                        className={cn("text-sm", {
+                            "text-red-500": state?.errors?.name,
+                            "text-slate-500": !state?.errors?.name,
+                        })}
+                    >
+                        {state?.errors?.name
+                            ? state.errors.name
+                            : "Nombre del tu producto (maximo 100 caracteres)"}
+                    </p>
+                </li>
+                <li>
+                    <Label className="font-semibold">Descripcion</Label>
+                    <Input
+                        name="description"
+                        placeholder="Product Description"
+                    />
+                    <p
+                        className={cn("text-sm", {
+                            "text-red-500": state?.errors?.description,
+                            "text-slate-500": !state?.errors?.description,
+                        })}
+                    >
+                        {state?.errors?.description
+                            ? state.errors?.description
+                            : "Descripción breve del producto (maximo 230 caracteres)"}
+                    </p>
+                </li>
+                <li>
+                    <Label className="font-semibold">Precio</Label>
+                    <Input name="price" placeholder="Product Price" />
+                    <p
+                        className={cn("text-sm", {
+                            "text-red-500": state?.errors?.price,
+                            "text-slate-500": !state?.errors?.price,
+                        })}
+                    >
+                        {state?.errors?.price
+                            ? state.errors.price
+                            : "El precio debe ser mayor a 0"}
+                    </p>
+                </li>
+            </ul>
 
             <Button className="mt-8" type="submit">
                 Añadir Producto
