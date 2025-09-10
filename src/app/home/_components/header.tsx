557eeff021f8f63a5ca81c1a72366e8746fb9fb2
ivanmayta <ivan.rodrigo.mayta@gmail.com>
2025-09-10 09:47:15 -0500

    refactor: replace magic URL with environment variable in Header component for improved configurability

--- src/app/home/_components/header.tsx
+++ src/app/home/_components/header.tsx
@@ -1,8 +1,8 @@
-import { NEXT_PUBLIC_APP_URL } from "@/config"
 import Link from "next/link"
 
 //TODO: fix magic url
 export default function Header() {
+    const appUrl = process.env.NEXT_PUBLIC_APP_URL
     return (
         <header className="h-16 my-4 sticky top-4  z-50">
             <div className="max-w-7xl mx-auto h-full flex items-center backdrop-blur-lg rounded-xl justify-between ">
@@ -39,12 +39,12 @@ export default function Header() {
                 <div className="flex items-center gap-4">
                     <div className=" hidden sm:flex items-center gap-4">
                         {/*<ExchangeBadge />*/}
-                        <a
+                        <Link
                             className="px-3 font-medium hover:bg-zinc-800 py-2 border-2 rounded-xl border-black bg-black text-white"
-                            href={`${NEXT_PUBLIC_APP_URL}`}
+                            href={`${appUrl}`}
                         >
                             Iniciar sesión
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
